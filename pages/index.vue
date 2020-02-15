f386f75890552c6482018522a587b19b38194450
I-CHIEN <peterpika21@gmail.com>
2020-02-15 14:35:01 +0800

    更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template lang="pug">
   div
-    div#map
+    div.bg__Block
+    h1.bg__text
+      p.bg__text--en Mask Map
+      p.bg__text--zh 
+        span.tx-primary 口罩
+        span 地圖
+
+    // 主畫面
+    section.wrap
+      // 左側列表
+      div
+        //- ul
+        //-   li(v-for="item in mapData")
+        //-     p {{ item.properties.name }}
+
+      // 地圖
+      div#map
 </template>
 
 <script>
@@ -8,11 +24,16 @@
 
 export default {
   data() {
-    return {}
+    return {
+      mapData: []
+    }
   },
   mounted() {
     this.$axios.get('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json').then(res => {
-      console.log(res)
+      if (res.status === 200) {
+        this.mapData = res.data.features
+        console.log(this.mapData)
+      }
     })
   }
 }
